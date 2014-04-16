8a209e0ebc122d484b3ac95882736a404757e46c
Akihiro Motoki <amotoki@gmail.com>
2014-04-17 00:02:24 +0900

    import-trans: Only fetch languages over 20% completion

--- import-trans.sh
+++ import-trans.sh
@@ -4,17 +4,21 @@
 ######################################################################
 
 HORIZON_REPO=/opt/stack/horizon
-TX_FORCE_FETCH=0
+TX_FORCE_FETCH=1
 TX_CMD=/usr/local/bin/tx
+TX_OPTS="-a --minimum-perc=20"
+DO_GIT_PULL=0
 
 if [ "$TX_FORCE_FETCH" -ne 0 ]; then
-  FORCE_OPT="-f"
+  TX_OPTS+=" -f"
 fi
 
 cd $HORIZON_REPO
-git pull
+if [ "$DO_GIT_PULL" -ne 0 ]; then
+  git pull
+fi
 
-$TX_CMD pull -a $FORCE_OPT
+$TX_CMD pull $TX_OPTS
 
 cd horizon
 ../manage.py compilemessages
@@ -23,8 +27,8 @@ cd openstack_dashboard
 ../manage.py compilemessages
 cd ..
 
-rm horizon/locale/en/LC_MESSAGES/django.mo
-rm horizon/locale/en/LC_MESSAGES/djangojs.mo
-rm openstack_dashboard/locale/en/LC_MESSAGES/django.mo
+rm -f horizon/locale/en/LC_MESSAGES/django.mo
+rm -f horizon/locale/en/LC_MESSAGES/djangojs.mo
+rm -f openstack_dashboard/locale/en/LC_MESSAGES/django.mo
 
 sudo service apache2 reload
