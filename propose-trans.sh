c1b2461634d5ed69f5444ba428175c2509960889
Akihiro Motoki <motoki@da.jp.nec.com>
2014-10-18 19:15:31 +0900

    Refactor propose-trans.sh

--- propose-trans.sh
+++ propose-trans.sh
@@ -2,63 +2,86 @@
 
 THRESH=95
 WORKDIR=$HOME/horizon
-BASE_BRANCH=proposed/juno
-WORK_BRANCH=translation-imports-for-juno
 RELEASE=juno
 
-INCLUDE_MO_FILE=0
+usage_exit() {
+    set +o xtrace
+    echo "Usage: $0 [options]"
+    echo ""
+    echo "Options:"
+    echo "  -r RELEASE : Specify release name in lower case (e.g. juno, icehouse, ...)"
+    echo "               (Default: $RELEASE)"
+    echo "  -b BRANCH  : Specify a target branch name."
+    echo "               If unspecified, it will be stable/RELEASE."
+    echo "               (Default: $BASE_BRANCH)"
+    echo "  -d WORKDIR : Horizon working git repo (Default: $WORKDIR)"
+    echo "  -m THRESH  : Minimum percentage of a translation (Default: $THRESH)"
+    exit 1
+}
+
+while getopts b:d:m:r:h OPT; do
+    case $OPT in
+        b) BASE_BRANCH=$OPTARG ;;
+        d) WORKDIR=$OPTARG ;;
+        m) THRESH=$OPTARG ;;
+        r) RELEASE=$OPTARG ;;
+        h) usage_exit ;;
+        \?) usage_exit ;;
+    esac
+done
+
+set -o xtrace
+
+if [ ! -n "$BASE_BRANCH" ]; then
+    BASE_BRANCH=stable/$RELEASE
+fi
+
+if [ $RELEASE = "icehouse" ]; then
+    INCLUDE_MO_FILE=1
+else
+    INCLUDE_MO_FILE=0
+fi
+
+export GIT_PAGER=
+export DJANGO_SETTINGS_MODULE=openstack_dashboard.test.settings
 
 TX_PROJECT=horizon
 TX_RESOURCES="horizon-translations-$RELEASE openstack-dashboard-translations-$RELEASE horizon-js-translations-$RELEASE"
 SOURCE_LANG=en
 TX_OPTS=-f
+WORK_BRANCH=translation-imports-for-$RELEASE
 
 TOP_DIR=$(cd $(dirname $0) && pwd)
 
-get_stats() {
-    local resource=$1
-    local ofile=/tmp/$$.$resource
-    tx --debug pull -r $resource -l $SOURCE_LANG | sed -n '/Statistics response is/,/^}$/p' | sed -e 's/^Statistics response is //' > $ofile
-    git checkout -- {horizon,openstack_dashboard}/locale/$SOURCE_LANG/LC_MESSAGES/*.po
-}
-
-get_langs_over_thresh() {
-    for resource in $TX_RESOURCES; do
-	get_stats $TX_PROJECT.$resource
-    done
-    $TOP_DIR/completion_check.py $THRESH /tmp/$$.$TX_PROJECT.*
-    rm -f /tmp/$$.$TX_PROJECT.*
-}
-
 setup_horizon_repo_if_nonexist() {
     if [ -d $WORKDIR ]; then
-	return
+        return
     fi
     git clone git://git.openstack.org/openstack/horizon.git $WORKDIR
 }
 
 setup_work_branch() {
     if `git branch | grep $WORK_BRANCH >/dev/null 2>&1`; then
-	git checkout $WORK_BRANCH
+        git checkout $WORK_BRANCH
     else
-	git checkout -b $WORK_BRANCH origin/$BASE_BRANCH
+        git checkout -b $WORK_BRANCH origin/$BASE_BRANCH
     fi
 }
 
 setup_tx_config() {
     if `grep translations-$RELEASE .tx/config >/dev/null`; then
-	# .tx/config already has resoruce entries for the targeted release
-	return
+        # .tx/config already has resoruce entries for the targeted release
+        return
     fi
     # If not, modify .tx/config from the existing .tx/config
     sed -i -e "s|translations\(-[a-z]\+\)\?\]$|translations-$RELEASE\]|" .tx/config
 }
 
 cleanup_message_catalogs() {
-    git reset HEAD -- horizon/locale/
-    git reset HEAD -- openstack_dashboard/locale/
-    git checkout -- horizon/locale/
-    git checkout -- openstack_dashboard/locale/
+    git reset -q HEAD -- horizon/locale/
+    git reset -q HEAD -- openstack_dashboard/locale/
+    git checkout -q -- horizon/locale/
+    git checkout -q -- openstack_dashboard/locale/
     git status | grep django.mo | xargs --no-run-if-empty rm
     git status | grep djangojs.mo | xargs --no-run-if-empty rm
     git status | grep django.po | xargs --no-run-if-empty rm
@@ -69,8 +92,92 @@ cleanup_message_catalogs() {
 remove_all_message_catalogs() {
     rm -rf openstack_dashboard/locale/*
     rm -rf horizon/locale/*
-    git checkout -- openstack_dashboard/locale/$SOURCE_LANG/
-    git checkout -- horizon/locale/$SOURCE_LANG/
+    git checkout -q -- openstack_dashboard/locale/$SOURCE_LANG/
+    git checkout -q -- horizon/locale/$SOURCE_LANG/
+}
+
+update_pot_files() {
+    ./run_tests.sh -q --makemessages
+    git add --all horizon/locale/
+    git add --all openstack_dashboard/locale/
+}
+
+compile_message_catalogs_if_necessary() {
+    if [ $INCLUDE_MO_FILE -eq 1 ]; then
+        ./run_tests.sh -q --compilemessages
+        # Add compiled message catalogs
+        git add --all horizon/locale/
+        git add --all openstack_dashboard/locale/
+    fi
+}
+
+remove_partial_languages() {
+    # Unless all of three resource (horizon/django,
+    # horizon/djangojs, openstack_dashboard.django) meet the threshold,
+    # remove these languages.
+    local lang
+    local all_langs=$((ls -1 openstack_dashboard/locale; ls -1 horizon/locale) | sort | uniq)
+    for lang in $all_langs; do
+        if [ -e "horizon/locale/$lang/LC_MESSAGES/django.po" \
+             -a -e "horizon/locale/$lang/LC_MESSAGES/djangojs.po" \
+             -a -e "openstack_dashboard/locale/$lang/LC_MESSAGES/django.po" ]; then
+            :
+        else
+            echo "Removing uncompleted language: $lang..."
+            rm -rf horizon/locale/$lang
+            rm -rf openstack_dashboard/locale/$lang
+        fi
+    done
+}
+
+filter_commits() {
+    # Don't include files where the only things which have changed are
+    # the creation date, the version number, the revision date,
+    # comment lines, or diff file information.
+    for f in `git diff --cached --name-only`; do
+        if [ ! -e "$f" ]; then
+            continue
+        fi
+        if [[ "$f" =~ "/django.mo" || "$f" =~ "/djangojs.mo" ]]; then
+            continue
+        fi
+        # It's ok if the grep fails
+        set +e
+        changed=$(git diff --cached "$f" \
+            | egrep -v "(POT-Creation-Date|Project-Id-Version|PO-Revision-Date|Last-Translator|Language-Team):" \
+            | egrep -c "^([-+][^-+#])")
+        set -e
+        if [ $changed -eq 0 ]; then
+            git reset -q "$f"
+            git checkout -- "$f"
+        fi
+    done
+}
+
+show_stats() {
+    local lang
+    local all_langs=$((ls -1 openstack_dashboard/locale; ls -1 horizon/locale) | sort | uniq)
+    for lang in $all_langs; do
+        if [ "$lang" = "$SOURCE_LANG" ]; then
+            continue
+        fi
+        echo "---------- $lang ----------"
+        echo -n "horizon: "
+        msgfmt -o /dev/null --statistics horizon/locale/$lang/LC_MESSAGES/django.po
+        echo -n "horizon javascript: "
+        msgfmt -o /dev/null --statistics horizon/locale/$lang/LC_MESSAGES/djangojs.po
+        echo -n "openstack_dashboard: "
+        msgfmt -o /dev/null --statistics openstack_dashboard/locale/$lang/LC_MESSAGES/django.po
+    done
+}
+
+show_add_delete_files() {
+    echo "------------------------------"
+    echo "Added files:"
+    git diff --cached --name-only --diff-filter=A
+    echo ""
+    echo "Deleted files:"
+    git diff --cached --name-only --diff-filter=D
 }
 
 setup_horizon_repo_if_nonexist
@@ -79,39 +186,18 @@ cleanup_message_catalogs
 setup_work_branch
 git status
 setup_tx_config
-if [ $INCLUDE_MO_FILE -ne 1 ]; then
-    remove_all_message_catalogs
-fi
+remove_all_message_catalogs
 
-echo "Checking translation statistics...."
-LANGS=$(get_langs_over_thresh | grep -vE "^$SOURCE_LANG\$")
-
-for lang in $LANGS; do
-    if [ "$lang" = "$SOURCE_LANG" ]; then
-	continue
-    fi
-    tx pull $TX_OPTS -l $lang
-done
-
-if [ $INCLUDE_MO_FILE -eq 1 ]; then
-    ./run_tests.sh --compilemessages
-fi
-
+update_pot_files
+tx pull -f -a --minimum-perc $THRESH
+remove_partial_languages
 git add --all horizon/locale/
 git add --all openstack_dashboard/locale/
+filter_commits
 
-for lang in $LANGS; do
-    if [ "$lang" = "$SOURCE_LANG" ]; then
-	continue
-    fi
-    echo "---------- $lang ----------"
-    echo -n "horizon: "
-    msgfmt -o /dev/null --statistics horizon/locale/$lang/LC_MESSAGES/django.po
-    echo -n "horizon javascript: "
-    msgfmt -o /dev/null --statistics horizon/locale/$lang/LC_MESSAGES/djangojs.po
-    echo -n "openstack_dashboard: "
-    msgfmt -o /dev/null --statistics openstack_dashboard/locale/$lang/LC_MESSAGES/django.po
-done
+compile_message_catalogs_if_necessary
+set +o xtrace
+show_stats
+show_add_delete_files
 
-echo
-echo "$(echo $LANGS | wc -w) languages have been imported."
+exit 0
