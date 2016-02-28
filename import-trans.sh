3c3dbdbb5129642efad760e36c739f9321f6233b
Akihiro Motoki <motoki@da.jp.nec.com>
2016-02-28 22:55:08 +0900

    import-trans.sh: support Zanata translation infrastructure

--- import-trans.sh
+++ import-trans.sh
@@ -3,15 +3,58 @@
 # Import translations from Transifex and reload Apache running Horizon
 ######################################################################
 
+ZANATA_VERSION=master
+
+. /usr/local/jenkins/slave_scripts/common_translation_update.sh
+
+pull_project() {
+    local project=$1
+    cd $DEVSTACK_DIR/$project
+    module_names=$(get_modulename $project django)
+    if [ ! -n "$module_names" ]; then
+        return
+    fi
+    setup_project $project $ZANATA_VERSION $module_names
+
+    zanata-cli -B pull -e --min-doc-percent 30
+
+    for module in "$module_names"; do
+        cd $module
+        DJANGO_SETTINGS_MODULE=openstack_dashboard.settings ../manage.py compilemessages
+        cd -
+    done
+}
+
+cleanup_project() {
+    local project=$1
+    cd $DEVSTACK_DIR/$project
+    git checkout -- horizon/locale/
+    git checkout -- openstack_dashboard/locale/
+    git status | grep django.mo | xargs --no-run-if-empty rm -v
+    git status | grep djangojs.mo | xargs --no-run-if-empty rm -v
+    git status | grep django.po | xargs --no-run-if-empty rm -v
+    git status | grep djangojs.po | xargs --no-run-if-empty rm -v
+    git status | grep /locale/ | xargs --no-run-if-empty rm -v -rf
+}
+
+update_project() {
+    local project=$1
+    cd $DEVSTACK_DIR/$project
+    if [ "$DO_GIT_PULL" -ne 0 ]; then
+        git branch --set-upstream-to=origin/$TARGET_BRANCH $TARGET_BRANCH
+        git checkout $TARGET_BRANCH
+        git pull
+        sudo pip install -e .
+    fi
+}
+
 logger -i -t `basename $0` "Started ($*)"
 
-RELEASE=juno
-HORIZON_REPO=/opt/stack/horizon
-TX_THRESH=30
-TX_FORCE_FETCH=1
+RELEASE=master
+ZANATA_VERSION=master
+DEVSTACK_DIR=/opt/stack
+THRESH=30
 
-#TX_CMD=/usr/local/bin/tx
-TX_OPTS="-a --minimum-perc=$TX_THRESH"
 DO_GIT_PULL=1
 
 usage_exit() {
@@ -19,21 +62,19 @@ usage_exit() {
     echo "Usage: $0 [options]"
     echo ""
     echo "Options:"
-    echo "  -r RELEASE : Specify release name in lower case (e.g. juno, icehouse, ...)"
+    echo "  -r RELEASE : Specify release name in lower case (e.g. liberty, mitaka, ...)"
     echo "               (Default: $RELEASE)"
     echo "  -b BRANCH  : Specify a target branch name."
     echo "               If unspecified, it will be stable/RELEASE."
     echo "               (Default: stable/$RELEASE)"
-    echo "  -d WORKDIR : Horizon working git repo (Default: $HORIZON_REPO)"
-    echo "  -m THRESH  : Minimum percentage of a translation (Default: $TX_THRESH)"
+    echo "  -m THRESH  : Minimum percentage of a translation (Default: $THRESH)"
     exit 1
 }
 
 while getopts b:d:m:r:h OPT; do
     case $OPT in
         b) TARGET_BRANCH=$OPTARG ;;
-        d) HORIZON_REPO=$OPTARG ;;
-        m) TX_THRESH=$OPTARG ;;
+        m) THRESH=$OPTARG ;;
         r) RELEASE=$OPTARG ;;
         h) usage_exit ;;
         \?) usage_exit ;;
@@ -43,73 +84,26 @@ done
 if [ ! -n "$TARGET_BRANCH" ]; then
   if [ "$RELEASE" = "master" ]; then
     TARGET_BRANCH=$RELEASE
+    ZANATA_VERSION=master
   else
     TARGET_BRANCH=stable/$RELEASE
+    ZANATA_VERSION=stable-$RELEASE
   fi
 fi
 
 set -o xtrace
 
-setup_tx_config() {
-    local release=$1
-    local slug
-    if [ "$release" = "master" ]; then
-      slug=translations
-    else
-      slug=translations-$release
-    fi
-    if `grep "$slug\]" .tx/config >/dev/null`; then
-        # .tx/config already has resoruce entries for the targeted release
-        return
-    fi
-    # If not, modify .tx/config from the existing .tx/config
-    sed -i -e "s|translations\(-[a-z]\+\)\?\]$|$slug\]|" .tx/config
-}
-
-TX_CMD=`which tx`
-if [ ! -n "$TX_CMD" ]; then
-  echo "Transifex 'tx' command not found"
+ZANATA_CMD=`which zanata-cli`
+if [ ! -n "$ZANATA_CMD" ]; then
+  echo "'$ZANATA_CMD' command not found"
   exit 1
 fi
 
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 
-if [ "$TX_FORCE_FETCH" -ne 0 ]; then
-  TX_OPTS+=" -f"
-fi
-
-cd $HORIZON_REPO
-
-git checkout -- .tx
-git checkout -- horizon/locale/
-git checkout -- openstack_dashboard/locale/
-git status | grep django.mo | xargs rm
-git status | grep djangojs.mo | xargs rm
-git status | grep django.po | xargs rm
-git status | grep djangojs.po | xargs rm
-git status | grep /locale/ | xargs rm -rf
-
-if [ "$DO_GIT_PULL" -ne 0 ]; then
-  git branch --set-upstream-to=origin/$TARGET_BRANCH $TARGET_BRANCH
-  git checkout $TARGET_BRANCH
-  git pull
-  sudo pip install -e .
-fi
-
-setup_tx_config $RELEASE
-
-$TX_CMD pull $TX_OPTS
-
-cd horizon
-../manage.py compilemessages
-cd ..
-cd openstack_dashboard
-../manage.py compilemessages
-cd ..
-
-rm -f horizon/locale/en/LC_MESSAGES/django.mo
-rm -f horizon/locale/en/LC_MESSAGES/djangojs.mo
-rm -f openstack_dashboard/locale/en/LC_MESSAGES/django.mo
+cleanup_project horizon
+update_project horizon
+pull_project horizon
 
 $TOP_DIR/update-lang-list.sh
 
