773c1fa92a1fdfa8e3e61ecd9c76c48fc7cf7d68
Akihiro Motoki <motoki@da.jp.nec.com>
2016-02-28 22:56:52 +0900

    update-lang-list.sh: remove unnecessary comments

--- update-lang-list.sh
+++ update-lang-list.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#SETTINGS=/opt/stack/horizon/openstack_dashboard/settings.py
 LOCAL_SETTINGS=/opt/stack/horizon/openstack_dashboard/local/local_settings.py
 
 TOP_DIR=$(cd $(dirname "$0") && pwd)
-#sed -i -e '/^LANGUAGES = /,/^)/d' $SETTINGS
-#python $TOP_DIR/update-lang-list.py >> $SETTINGS
 
 sed -i -e '/^LANGUAGES = /,$d' $LOCAL_SETTINGS
 python $TOP_DIR/update-lang-list.py >> $LOCAL_SETTINGS
